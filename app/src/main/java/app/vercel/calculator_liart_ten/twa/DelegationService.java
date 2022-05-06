dcd88410b26a08c4ed2829fed3acf14b279a68ca
Merzaad <Merzaad@gmail.com>
2022-05-06 09:37:50 +0430

    manifest

---
+++ app/src/main/java/app/vercel/calculator_liart_ten/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.calculator_liart_ten.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
